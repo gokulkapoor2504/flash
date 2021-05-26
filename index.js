af13db4008b3b67934022046b65fa8a3cf67475a
gokulkapoor2504 <gokulkapoor123@gmail.com>
2021-05-26 18:39:12 +0530

    java included

---
+++ index.js
@@ -0,0 +1,48 @@
+var i=0;
+function read(){
+    if(!i){
+        document.getElementById("more").style.display="inline";
+        document.getElementById("dots").style.display="none";
+        document.getElementById("read more").innerHTML="Read less";
+        i=1;
+    }
+    else{
+        document.getElementById("more").style.display="none";
+        document.getElementById("dots").style.display="inline";
+        document.getElementById("read more").innerHTML="Continue Reading";
+        i=0;
+
+    }
+}
+var i=0;
+function read1(){
+    if(!i){
+        document.getElementById("more1").style.display="inline";
+        document.getElementById("dots1").style.display="none";
+        document.getElementById("read more1").innerHTML="Read less";
+        i=1;
+    }
+    else{
+        document.getElementById("more1").style.display="none";
+        document.getElementById("dots1").style.display="inline";
+        document.getElementById("read more1").innerHTML="Continue Reading";
+        i=0;
+
+    }
+}
+var i=0;
+function read2(){
+    if(!i){
+        document.getElementById("more2").style.display="inline";
+        document.getElementById("dots2").style.display="none";
+        document.getElementById("read more2").innerHTML="Read less";
+        i=1;
+    }
+    else{
+        document.getElementById("more2").style.display="none";
+        document.getElementById("dots2").style.display="inline";
+        document.getElementById("read more2").innerHTML="Continue Reading";
+        i=0;
+
+    }
+}
